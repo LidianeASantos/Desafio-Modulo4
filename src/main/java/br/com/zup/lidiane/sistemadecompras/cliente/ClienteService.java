513f1ab7b27996534c4944fe2737dc0f4b39c79d
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-12 16:44:32 -0300

    Criar método de cadastro de Cliente na Classe Service

--- src/main/java/br/com/zup/lidiane/sistemadecompras/cliente/ClienteService.java
+++ src/main/java/br/com/zup/lidiane/sistemadecompras/cliente/ClienteService.java
@@ -2,7 +2,15 @@ package br.com.zup.lidiane.sistemadecompras.cliente;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ClienteService {
-    
+
+    private List<Cliente> clientes = new ArrayList<>();
+
+    public void adicionarClienteNaLista(Cliente cliente){
+        clientes.add( cliente );
+    }
 }
