f61732afc7373958f3f05908f1cef147f3ee4679
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-12 15:17:47 -0300

    Criar Classe Service Produto

---
+++ src/main/java/br/com/zup/lidiane/sistemadecompras/produto/ProdutoService.java
@@ -0,0 +1,14 @@
+package br.com.zup.lidiane.sistemadecompras.produto;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    private List<Produto> produtos = new ArrayList<>();
+
+   
+}
