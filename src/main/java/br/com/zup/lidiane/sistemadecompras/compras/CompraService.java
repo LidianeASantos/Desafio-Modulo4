a7f8c6f06205950bc8755a4bfc329cac41fe90cd
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-12 17:47:44 -0300

    Criar classe Service de Compra

---
+++ src/main/java/br/com/zup/lidiane/sistemadecompras/compras/CompraService.java
@@ -0,0 +1,12 @@
+package br.com.zup.lidiane.sistemadecompras.compras;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CompraService {
+
+    private List<Compra> compras = new ArrayList<>();
+}
