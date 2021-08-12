e078560fcf674bc69bce0db17634bd0aa24fc6eb
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-12 15:37:08 -0300

    Criar cadastro de Produto na Classe Controller

--- src/main/java/br/com/zup/lidiane/sistemadecompras/produto/ProdutoController.java
+++ src/main/java/br/com/zup/lidiane/sistemadecompras/produto/ProdutoController.java
@@ -1,8 +1,10 @@
 package br.com.zup.lidiane.sistemadecompras.produto;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -10,4 +12,10 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarProduto(@RequestBody @Valid Produto produto){
+        produtoService.adicionarProdutoNaLista( produto );
+    }
 }
