48d3a564330793ba5d7496c7407246e2523446f5
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-12 13:42:52 -0300

    Setup Projeto de Compras

---
+++ src/main/java/br/com/zup/lidiane/sistemadecompras/SistemadecomprasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.lidiane.sistemadecompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadecomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadecomprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/lidiane/sistemadecompras/SistemadecomprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.lidiane.sistemadecompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadecomprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
