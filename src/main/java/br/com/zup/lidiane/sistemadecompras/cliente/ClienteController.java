059fc65f83c67007d2ea735526bd2c59f8a0653d
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-12 17:11:24 -0300

    Implementando cadastro de Cliente na Controller

--- src/main/java/br/com/zup/lidiane/sistemadecompras/cliente/ClienteController.java
+++ src/main/java/br/com/zup/lidiane/sistemadecompras/cliente/ClienteController.java
@@ -1,8 +1,10 @@
 package br.com.zup.lidiane.sistemadecompras.cliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/clientes")
@@ -10,4 +12,10 @@ public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarCliente(@RequestBody @Valid Cliente cliente){
+        clienteService.adicionarClienteNaLista( cliente );
+    }
 }
