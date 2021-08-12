c75b54585000baf2253e0c57eebaeca2310e900e
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-12 16:35:30 -0300

    Implementar retorno da lista na Classe Controller de Produto

--- src/main/java/br/com/zup/lidiane/sistemadecompras/produto/ProdutoController.java
+++ src/main/java/br/com/zup/lidiane/sistemadecompras/produto/ProdutoController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -17,5 +18,10 @@ public class ProdutoController {
     @ResponseStatus(HttpStatus.CREATED)
     public void cadastrarProduto(@RequestBody @Valid Produto produto){
         produtoService.adicionarProdutoNaLista( produto );
+    }
+
+    @GetMapping
+    public List<Produto> visualizarProduto(){
+        return produtoService.retornarProdutoNaLista();
     }
 }
